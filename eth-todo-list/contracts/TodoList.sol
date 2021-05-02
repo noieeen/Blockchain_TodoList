4f2ca7fe405c60298a65acc33f975cb49a454627
Noieeen <tharnnn1401@gmail.com>
2021-05-03 02:05:34 +0700

    require not define

--- eth-todo-list/contracts/TodoList.sol
+++ eth-todo-list/contracts/TodoList.sol
@@ -1,5 +1,21 @@
 pragma solidity >=0.4.22 <0.9.0;
 
-contract TodoList{
-    uint public TaskCount = 0;
+contract TodoList {
+    uint256 public TaskCount = 0;
+
+    struct Task {
+        uint256 id;
+        string content;
+        bool completed;
+    }
+    mapping(uint256 => Task) public tasks;
+
+    constructor() public {
+        createTask("Check out Block Shop");
+    }
+
+    function createTask(string memory _content) public {
+        TaskCount++;
+        tasks[TaskCount] = Task(TaskCount, _content, false);
+    }
 }
